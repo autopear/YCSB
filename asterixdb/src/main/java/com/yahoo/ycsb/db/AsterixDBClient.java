fa6fbd51f7a2b48c9f0c508b06517ee7c94f5e87
Merlin <lonelystars@gmail.com>
2018-04-18 22:17:16 -0700

    Little update

--- asterixdb/src/main/java/com/yahoo/ycsb/db/AsterixDBClient.java
+++ asterixdb/src/main/java/com/yahoo/ycsb/db/AsterixDBClient.java
@@ -20,14 +20,63 @@ import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.Status;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 public class AsterixDBClient extends DB {
 
+  public static final String DB_URL = "db.url";
+
+  private String SERVICE_URL;
+
   public void init() throws DBException {
+    SERVICE_URL = getProperties().getProperty(DB_URL, "http://localhost:19002/query/service");
+  }
+
+  private HttpPost generatePost(final String query) throws UnsupportedEncodingException {
+    HttpPost post = new HttpPost(SERVICE_URL);
+    List<NameValuePair> params = new ArrayList<>();
+    params.add(new BasicNameValuePair("statement", query));
+    params.add(new BasicNameValuePair("mode", "immediate"));
+    post.setEntity(new UrlEncodedFormEntity(params));
+    return post;
+  }
+
+  private boolean queryWithoutResult(final String query) {
+    HttpPost post;
+    try {
+      post = generatePost(query);
+    } catch (UnsupportedEncodingException ex) {
+      return false;
+    }
+
+    CloseableHttpResponse response;
+    CloseableHttpClient client = HttpClients.createDefault();
+    try {
+      response = client.execute(post);
+    } catch (ClientProtocolException ex) {
+      return false;
+    } catch (IOException ex) {
+      return false;
+    } finally {
+      try {
+        client.close();
+      } catch (IOException ex) { }
+    }
+
+    StatusLine s = response.getStatusLine();
+    return (s.getStatusCode() == 200);
   }
 
   public Status scan(String table, String startkey, int recordcount, Set<String> fields,
